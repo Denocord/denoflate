e3b6de761cb39e9911f10f8254476ba66e26d483
TTtie <tttiett@outlook.com>
2020-08-20 19:43:00 +0200

    Make development builds detectable

--- mod.ts
+++ mod.ts
@@ -3,6 +3,7 @@ import {
 } from "https://raw.githubusercontent.com/Denocord/deno-plugin-prepare/528acc01d5c8468ea29db7638957461c498427a0/mod.ts";
 
 export const VERSION = "0.3.0";
+export const IS_DEV = true;
 
 interface IDecompressor {
   push(buf: Uint8Array, flush?: boolean): void;
@@ -13,10 +14,10 @@ let Decompressor: IDecompressor;
 
 //@ts-ignore
 if (typeof Deno.openPlugin === "function") {
-  const IS_DEV = false;
   let url =
     `https://github.com/Denocord/denoflate/releases/download/v${VERSION}`;
   if (IS_DEV) url = `${import.meta.url}/../target/release`;
+
   await prepare({
     name: "denoflate",
     printLog: false,
@@ -55,7 +56,7 @@ if (typeof Deno.openPlugin === "function") {
     `The native plugin couldn't be loaded because of disabled unstable APIs. Please run Deno again with the --unstable flag.`,
   );
   Decompressor = {
-    push(_buf: Uint8Array, _flush = false) {
+    push(_: Uint8Array, __ = false) {
       throw NATIVE_PLUGIN_ERROR;
     },
 
