e1d365e8a8727761476f0134457172ed5a355a9a
TTtie <tttiett@outlook.com>
2020-08-22 21:25:10 +0200

    Use reset() to reset context, 100% safe
    No more unsafe blocks 🎉

--- src/lib.rs
+++ src/lib.rs
@@ -2,36 +2,33 @@ use deno_core::plugin_api::{Interface, ZeroCopyBuf, Op};
 use inflate::InflateWriter;
 
 use std::io::prelude::*;
-static mut DECOMPRESS: Option<InflateWriter<Vec<u8>>> = None;
+use std::sync::Mutex;
+
+thread_local! {
+    static DECOMPRESS: Mutex<InflateWriter<Vec<u8>>> = 
+        Mutex::new(InflateWriter::from_zlib(vec![]));
+}
 
 #[no_mangle]
 pub fn deno_plugin_init(iface: &mut dyn Interface) {
-    unsafe {
-        DECOMPRESS = Some(InflateWriter::from_zlib(Vec::new()));
-    }
-    
     iface.register_op("denoflate::push", push);
     iface.register_op("denoflate::flush", flush);
     iface.register_op("denoflate::reset", reset);
 }
 
-fn reset(___: &mut dyn Interface, _: &mut [ZeroCopyBuf]) -> Op {
-    unsafe {
-        DECOMPRESS = Some(InflateWriter::from_zlib(Vec::new()));
-    }
-
+fn reset(_: &mut dyn Interface, _: &mut [ZeroCopyBuf]) -> Op {
+    DECOMPRESS.with(|d| d.lock().unwrap().reset());
     Op::Sync(Vec::new().into_boxed_slice())
 }
 
-fn push(_iface: &mut dyn Interface, data: &mut [ZeroCopyBuf]) -> Op {
-    let decompresser = unsafe { DECOMPRESS.as_mut().unwrap() };
+fn push(_: &mut dyn Interface, data: &mut [ZeroCopyBuf]) -> Op {
     let true_data = &data[0];
-    decompresser.write(true_data).unwrap();
+    DECOMPRESS.with(|d| d.lock().unwrap().write(true_data).unwrap());
     Op::Sync(Vec::new().into_boxed_slice())
 }
 
-fn flush(_iface: &mut dyn Interface, _: &mut [ZeroCopyBuf]) -> Op {
-    let decompresser = unsafe { DECOMPRESS.as_mut().unwrap() };
-    let buf = decompresser.finish_mut().unwrap();
+fn flush(_: &mut dyn Interface, _: &mut [ZeroCopyBuf]) -> Op {
+    let buf = DECOMPRESS.with(|d|
+        d.lock().unwrap().finish_mut().unwrap());
     Op::Sync(buf.into_boxed_slice())
 }
